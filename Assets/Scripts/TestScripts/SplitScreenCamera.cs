115fcacbf59205715521f8d150a01b1d9f510b26
b24leoma <b24leoma@student.his.se>
2026-01-30 14:41:07 +0100

    splitscrren still broken

---
+++ Assets/Scripts/TestScripts/SplitScreenCamera.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof(Camera))]
+public class SplitScreenCamera : MonoBehaviour
+{
+    private Camera cam;
+    private int index;
+    private int totalPlayers;
+
+    private void Awake()
+    {
+        PlayerInputManager.instance.onPlayerJoined += HandlePlayerJoined;
+    }
+
+    private void HandlePlayerJoined(PlayerInput obj)
+    {
+        totalPlayers = PlayerInput.all.Count;
+        SetupCamera();
+    }
+
+    private void Start()
+    {
+        index = GetComponentInParent<PlayerInput>().playerIndex;
+        totalPlayers = PlayerInput.all.Count;
+        cam = GetComponent<Camera>();
+        cam.depth = index;
+
+        SetupCamera();
+    }
+
+    private void SetupCamera()
+    {
+        switch (totalPlayers)
+        {
+            case 1:
+                cam.rect = new Rect(0, 0, 1, 1);
+                break;
+            case 2:
+                cam.rect = new Rect(index == 0 ? 0 : 0.5f, 0, 0.5f, 1);
+                break;
+            case >= 3:
+                cam.rect = new Rect(
+                    (index % 2) * 0.5f,
+                    (index < 2) ? 0.5f : 0,
+                    0.5f,
+                    0.5f);
+                break;
+        }
+    }
+}
