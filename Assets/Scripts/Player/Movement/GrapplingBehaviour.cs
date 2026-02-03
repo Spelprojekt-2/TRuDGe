6e4f070c4419e5f6e36f7ec1a3f753f83be96ca0
Ruby <ruby.calmerman@gmail.com>
2026-02-03 15:55:27 +0100

    messy graphical representation

--- Assets/Scripts/Player/Movement/GrapplingBehaviour.cs
+++ Assets/Scripts/Player/Movement/GrapplingBehaviour.cs
@@ -1,10 +1,11 @@
 using UnityEditor.EditorTools;
 using UnityEngine;
 
-
+[RequireComponent(typeof(LineRenderer))]
 public class GrapplingBehaviour : MonoBehaviour
 {
     #region Component refs
+    private LineRenderer lineRenderer;
     [SerializeField] private Rigidbody rigidbody;
     [Tooltip("The object that will follow the grapple hook's azimuth/heading/yaw rotation (grapple turret)")]
     [SerializeField] private Transform grappleAzimuthObject;
@@ -14,5 +15,23 @@ public class GrapplingBehaviour : MonoBehaviour
     [SerializeField] private Vector3 grappleMuzzleOffset = Vector3.zero;
     #endregion
 
-    
+    [SerializeField] private Vector3 lRPoint = Vector3.zero;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        lineRenderer.SetPosition(0, grappleElevationObject.TransformPoint(grappleMuzzleOffset));
+        lineRenderer.SetPosition(1, lRPoint);
+
+        grappleElevationObject.LookAt(lRPoint);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(grappleElevationObject.position + grappleMuzzleOffset, 0.1f);
+    }
 }
