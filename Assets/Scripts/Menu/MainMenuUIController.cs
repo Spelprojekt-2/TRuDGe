5cf41054a152a638587d2a6bcf5a14e367665743
b24leoma <b24leoma@student.his.se>
2026-02-07 02:35:07 +0100

    main menu

--- Assets/Scripts/Menu/MainMenuUIController.cs
+++ Assets/Scripts/Menu/MainMenuUIController.cs
@@ -1,15 +1,67 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenuUIController : MonoBehaviour
 {
-    [SerializeField] private GameObject FirstJoinPopup;
+    [SerializeField] private GameObject firstJoinPopup;
+    [SerializeField] private Button[] mainMenuButtons;
+    [SerializeField] private GameObject singlePlayerButtons;
+    [SerializeField] private GameObject multiplayerButtons;
+    [SerializeField] private GameObject settingsMenu;
     public void ShowJoinPopup()
     {
-        FirstJoinPopup.SetActive(true);
+        firstJoinPopup.SetActive(true);
     }
 
     public void HideJoinPopup()
     {
-        FirstJoinPopup.SetActive(false);
+        firstJoinPopup.SetActive(false);
+    }
+
+    public void ShowSinglePlayerMenu()
+    {
+        singlePlayerButtons.SetActive(true);
+        for (int i = 0; i < mainMenuButtons.Length; i++)
+        {
+            mainMenuButtons[i].enabled = false;
+        }
+        SelectObject(singlePlayerButtons.GetComponentInChildren<Button>().gameObject);
+    }
+    public void ShowMultiplayerMenu()
+    {
+        multiplayerButtons.SetActive(true);
+        for (int i = 0; i < mainMenuButtons.Length; i++)
+        {
+            mainMenuButtons[i].enabled = false;
+        }
+        SelectObject(multiplayerButtons.GetComponentInChildren<Button>().gameObject);
+    }
+
+    public void ShowSettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+        for (int i = 0; i < mainMenuButtons.Length; i++)
+        {
+            mainMenuButtons[i].enabled = false;
+        }
+        SelectObject(settingsMenu.GetComponentInChildren<Button>().gameObject);
+    }
+
+    public void SelectMainMenu(int buttonToSelect)
+    {
+        singlePlayerButtons.SetActive(false);
+        multiplayerButtons.SetActive(false);
+        settingsMenu.SetActive(false);
+        for (int i = 0; i < mainMenuButtons.Length; i++)
+        {
+            mainMenuButtons[i].enabled = true;
+        }
+        SelectObject(mainMenuButtons[buttonToSelect].gameObject);
+    }
+
+    void SelectObject(GameObject go)
+    {
+        EventSystem.current.SetSelectedGameObject(go);
     }
 }
