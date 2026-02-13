a878ef55ab21aaa2d34aff3a7b0510c55a5a6441
d21oskvi <d21oskvi@student.his.se>
2026-02-13 16:24:45 +0100

    Updated banks & some C# script changes.

--- Assets/Audio/Scripts/InteractablesAudio.cs
+++ Assets/Audio/Scripts/InteractablesAudio.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu (menuName = "Scriptables/Audio/Interactables")]
 public class InteractablesAudio : ScriptableObject
 {
     #region EventReferences
-    [SerializeField] private EventReference PowerupPickupEvent;
+    [SerializeField] private EventReference GasolinePickupEvent;
     #endregion
+
+    public void PlayGasolinePickup(GameObject gasolinePickup)
+    {
+        Debug.LogWarning("PlayGasolinePickup!");
+        RuntimeManager.PlayOneShot(GasolinePickupEvent, gasolinePickup.transform.position);
+    }
 }
