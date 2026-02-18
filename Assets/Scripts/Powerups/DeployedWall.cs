f1dd6cac8371df0baece4a3db0d1be3d3c309ee0
Sebastian <sebastian.stavenek@gmail.com>
2026-02-18 15:32:58 +0100

    bh

--- Assets/Scripts/Powerups/DeployedWall.cs
+++ Assets/Scripts/Powerups/DeployedWall.cs
@@ -43,7 +43,7 @@ public class WallChildListener : MonoBehaviour
         myIndex = index;
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         mainScript.OnChildHit(myIndex);
     }
