b8befa4d01f1e5d09f2443be58b459f27cb28269
Busmossan <a24emimo@student.his.se>
2026-02-18 15:16:52 +0100

    subtitles start

--- Assets/Scripts/Menu/VideoPlay/VideoManager.cs
+++ Assets/Scripts/Menu/VideoPlay/VideoManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class VideoManager : MonoBehaviour
 {
+    [Header ("Videos")]
     public VideoPlayer video1;
     public VideoPlayer video2;
     public VideoPlayer video3;
@@ -13,6 +15,10 @@ public class VideoManager : MonoBehaviour
     public VideoPlayer video6;
     public AudioSource announcment;
 
+    [Header("Subtitles")]
+    public TextMeshProUGUI subtitlesText;
+
+    [Header("Scene")]
     public string SwitchScene = "Level1_sloped";
 
     void Update()
@@ -28,6 +34,8 @@ public class VideoManager : MonoBehaviour
         announcment.Play();
         video1.Play();
         video1.loopPointReached += SwapVideo1;
+
+        subtitlesText.text = "Schlammrennstrecke, a mud track located in a beautiful German beech forest. It features tight turns, a few tricky obstacles and ending off with another right turn toward the goal. Schlammrennstrecke was originally designed for drifting with cars but unfortunately most of it was destroyed during “The Battle of Schlamm” in the war. In 2055 it was repaired, forming the amazing track we see today!";
     }
     public void SwapVideo1(VideoPlayer vp)
     {
