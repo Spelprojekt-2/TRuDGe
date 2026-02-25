7ddb08f18d43c3c4250fa3124a5c93abc54d7505
b24leoma <b24leoma@student.his.se>
2026-02-25 16:25:52 +0100

    pickup bug fix i guess

--- Assets/Scripts/Powerups/Pickup.cs
+++ Assets/Scripts/Powerups/Pickup.cs
@@ -86,6 +86,7 @@ public class Pickup : MonoBehaviour
     public IEnumerator DroppedTanks()
     {
         canRespawn = false;
+        if (col == null) yield break;
         col.enabled = false;
         for (int i = 0; i < 3; i++)
         {
