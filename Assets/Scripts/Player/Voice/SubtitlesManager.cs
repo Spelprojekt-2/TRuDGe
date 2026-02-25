0d20fd32c340b0d07fdab0dcb7f2b221623f96d6
Busmossan <a24emimo@student.his.se>
2026-02-25 15:44:44 +0100

    continued work on subtitles

--- Assets/Scripts/Player/Voice/SubtitlesManager.cs
+++ Assets/Scripts/Player/Voice/SubtitlesManager.cs
@@ -6,61 +6,57 @@ public class SubtitlesManager : MonoBehaviour
 {
     public GameObject subtitlePrefab;
     public Transform subtitleParent;
-    public float subtitleDuration = 5f;
+    //public float subtitleDuration = 5f;
     void Awake()
     {
-        //Image image = subtitlePrefab.GetComponentInChildren<Image>();
-
         Napoleon_Carla_Crash();
     }
-    void SpawnSubtitle(string subtext)
+    void SpawnSubtitle(string subtext, float duration)
     {
-        GameObject newSubtitle = Instantiate(subtitlePrefab, subtitleParent);
-        RectTransform rect = newSubtitle.GetComponent<RectTransform>();
-        rect.localScale = Vector3.one;
-        rect.anchoredPosition = Vector2.zero;
+        GameObject newSubtitle = Instantiate(subtitlePrefab, subtitleParent, false);
+        newSubtitle.SetActive(true);
 
         TextMeshProUGUI text = newSubtitle.GetComponentInChildren<TextMeshProUGUI>();
         text.text = subtext;
 
-        Destroy(newSubtitle, subtitleDuration);
+        Destroy(newSubtitle, duration);
     }
 
     // ======== Voice line subtitles =========
     //Lars-Göran
     public void Lars_Start()
     {
-        SpawnSubtitle("I hope no one decides to cheat today!");
+        SpawnSubtitle("I hope no one decides to cheat today!", 5f);
     }
 
     //Napoleon
     public void Napoleon_DriveBy()
     {
-        SpawnSubtitle("Napoleon drive-by");
+        SpawnSubtitle("Napoleon drive-by", 5f);
     }
     public void Napoleon_DrivenBy()
     {
-        SpawnSubtitle("Napoleon driven by");
+        SpawnSubtitle("Napoleon driven by", 5f);
     }
     public void Napoleon_Carla_Crash()
     {
-        SpawnSubtitle("<color=grey>[</color><color=blue>Carla</color><color=grey>]</color> Go away!");
-        Invoke(nameof(NapoleonReply_Carla_Crash), 5f);
+        SpawnSubtitle("<color=grey>[</color><color=blue>Carla</color><color=grey>]</color> Go away!", 5f);
+        Invoke(nameof(NapoleonReply_Carla_Crash), 3f);
     }
     void NapoleonReply_Carla_Crash()
     {
-        SpawnSubtitle("<color=grey>[</color><color=blue>Napoleon</color><color=grey>]</color> No u.");
+        SpawnSubtitle("<color=grey>[</color><color=blue>Napoleon</color><color=grey>]</color> No u.", 3f);
     }
 
     //Nina
     public void Nina_Hit()
     {
-        SpawnSubtitle("Nina_hit");
+        SpawnSubtitle("Nina_hit", 5f);
     }
 
     //Carla
     public void Carla_Win()
     {
-        SpawnSubtitle("Carla win");
+        SpawnSubtitle("Carla win", 5f);
     }
 }
