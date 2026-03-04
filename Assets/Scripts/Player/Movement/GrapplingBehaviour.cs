1fa285342469184dd9ad27bce740b67afda3e094
Ruby <ruby.calmerman@gmail.com>
2026-03-04 18:27:52 +0100

    grapple lock should now stop re-grappling

--- Assets/Scripts/Player/Movement/GrapplingBehaviour.cs
+++ Assets/Scripts/Player/Movement/GrapplingBehaviour.cs
@@ -36,7 +36,7 @@ public class GrapplingBehaviour : MonoBehaviour
     {
         if (context.performed)
         {
-            if (isInGrappleRange) StartGrappple();
+            if (isInGrappleRange && !isGrappling) StartGrappple();
         }
         else if (context.canceled)
         {
@@ -157,7 +157,11 @@ public class GrapplingBehaviour : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (!isGrappling || grappleable == null) return;
+        if (!isGrappling || grappleable == null)
+        {
+            if (isGrappling) EndGrapple(false);
+            return;
+        }
 
         cachedGrapplePoint = grappleable.GetGrapplePoint(this);
 
