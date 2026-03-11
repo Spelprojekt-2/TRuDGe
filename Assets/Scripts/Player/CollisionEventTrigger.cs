485d651b8afbf79d8eb48c61f4da0a92c641e4a6
Ruby <ruby.calmerman@gmail.com>
2026-03-11 17:33:53 +0100

    Revert "add input buffer"
    This reverts commit 3fc514ac0b1e69fd440867500b05d88178ba8ac3.

--- Assets/Scripts/Player/CollisionEventTrigger.cs
+++ Assets/Scripts/Player/CollisionEventTrigger.cs
@@ -3,21 +3,15 @@ using UnityEngine.Events;
 
 public class CollisionEventTrigger : MonoBehaviour
 {
-    [SerializeField] private int wallLayer;
+    [SerializeField] private LayerMask wallLayer;
     [SerializeField] private UnityEvent hitWall;
-    [SerializeField] private int playerLayer;
+    [SerializeField] private LayerMask playerLayer;
     [SerializeField] private UnityEvent hitPlayer;
     
     private void OnCollisionEnter(Collision collision)
     {
         // layermasks aren't constant and can't be used for a switch
-        if (collision.gameObject.layer == wallLayer)
-        {
-            hitWall.Invoke();
-        }
-        if (collision.gameObject.layer == playerLayer)
-        {
-            hitPlayer.Invoke();
-        }
+        if (collision.gameObject.layer == wallLayer.value) hitWall.Invoke();
+        if (collision.gameObject.layer == playerLayer.value) hitPlayer.Invoke();
     }
 }
