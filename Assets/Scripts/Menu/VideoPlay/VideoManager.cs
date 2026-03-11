fd375a56cd775a65d7ac227a242bbe02db7e1992
Busmossan <a24emimo@student.his.se>
2026-03-11 14:24:02 +0100

    gg

--- Assets/Scripts/Menu/VideoPlay/VideoManager.cs
+++ Assets/Scripts/Menu/VideoPlay/VideoManager.cs
@@ -18,16 +18,17 @@ public class VideoManager : MonoBehaviour
     
     void Start()
     {
+        /*switch (SceneManager.GetActiveScene().name)
+        {
+            case "Level1 Ann.": AudioManager.instance.PlayIntroSchlammenstrecke; break;
+            case "Level2 Ann.": break;
+            case "Level3 Ann.": break;
+        }*/
         voiceAudio.PlayAnnouncement();
         video.Play();
         video.loopPointReached += SwapVideo;
 
-        /*switch (SceneManager.GetActiveScene().name)
-        {
-            case "Level1 Ann.": AudioManager.instance.Schlamm; break;
-            case "Level2 Ann.": break;
-            case "Level3 Ann.": break;
-        }*/
+        
        
         
         //subtitlesText.text = "Schlammrennstrecke, a mud track located in a beautiful German beech forest. It features tight turns, a few tricky obstacles and ending off with another right turn toward the goal. Schlammrennstrecke was originally designed for drifting with cars but unfortunately most of it was destroyed during “The Battle of Schlamm” in the war. In 2055 it was repaired, forming the amazing track we see today!";
