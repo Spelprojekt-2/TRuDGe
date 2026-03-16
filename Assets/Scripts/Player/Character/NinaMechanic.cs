dd5533a505b69261e5251d5655976126fd9f605e
Busmossan <a24emimo@student.his.se>
2026-03-16 15:21:43 +0100

    speed lines

--- Assets/Scripts/Player/Character/NinaMechanic.cs
+++ Assets/Scripts/Player/Character/NinaMechanic.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NinaMechanic : MonoBehaviour
 {
     [SerializeField] private ParticleSystem steam;
+    [SerializeField] private Camera renderingCamera;
     [SerializeField] private bool specialEnabled;
     
     
@@ -21,17 +22,45 @@ public class NinaMechanic : MonoBehaviour
     }
     void OnSceneLoaded()
     {
+        RacerData data = transform.root.GetComponentInChildren<RacerData>();
+        
         if (!SceneController.instance.IsMenu)
         {
-            RacerData data = transform.root.GetComponentInChildren<RacerData>();
             if (data.racername == "The Brass Beast")
             {
                 specialEnabled = true;
+            }
+
+            int layer;
+            int playerIndex = data.index;
+            switch (playerIndex)
+            {
+                case 0:
+                    layer = LayerMask.NameToLayer("Player1fx");
+                    steam.gameObject.layer = layer;
+                    renderingCamera.cullingMask |= 1 << layer;
+                    break;
+                case 1: steam.gameObject.layer = LayerMask.NameToLayer("Player2fx"); 
+                    layer = LayerMask.NameToLayer("Player2fx");
+                    steam.gameObject.layer = layer;
+                    renderingCamera.cullingMask |= 1 << layer;
+                    break;
+                case 2: steam.gameObject.layer = LayerMask.NameToLayer("Player3fx"); 
+                    layer = LayerMask.NameToLayer("Player3fx");
+                    steam.gameObject.layer = layer;
+                    renderingCamera.cullingMask |= 1 << layer;
+                    break;
+                case 3: steam.gameObject.layer = LayerMask.NameToLayer("Player4fx"); 
+                    layer = LayerMask.NameToLayer("Player4fx");
+                    steam.gameObject.layer = layer;
+                    renderingCamera.cullingMask |= 1 << layer;
+                    break;
             }
         }
         if (SceneController.instance.IsMenu)
         {
             specialEnabled = false;
+            steam.Stop();
         }
     }
     public void VentSteam()
