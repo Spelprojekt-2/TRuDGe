94a96cae7ee33d2ac9c43da5ad41ab2feaa01312
d21oskvi <d21oskvi@student.his.se>
2026-03-17 20:18:29 +0100

    Turbo + Wall placement audio

--- Assets/Scripts/Player/PlayerPowerups.cs
+++ Assets/Scripts/Player/PlayerPowerups.cs
@@ -143,14 +143,12 @@ public class PlayerPowerups : MonoBehaviour
 
             case PowerUpType.smoke:
                 onSmoke.Invoke();
-                playerAudio.PlaySmokeAudio(transform.position); // Play smoke audio
                 Smokescreen();
                 break;
 
             case PowerUpType.landMine:
                 onLandmine.Invoke();
                 GameObject landmine = Instantiate(landMine, transform.position, Quaternion.identity);
-                playerAudio.PlayLandminePlaceAudio(landmine); // Play landmine audio
                 break;
 
             case PowerUpType.airstrike:
@@ -165,7 +163,6 @@ public class PlayerPowerups : MonoBehaviour
 
             case PowerUpType.scatterShot:
                 onScatterShot.Invoke();
-                playerAudio.PlayScatterShotAudio();
                 GameObject scatterShotSpawned = Instantiate(scatterShot, new Vector3(transform.position.x, transform.position.y + 5f, transform.position.z), Quaternion.LookRotation(transform.forward));
                 foreach (var projectile in scatterShotSpawned.GetComponentsInChildren<Projectile>())
                 {
@@ -311,6 +308,9 @@ public class PlayerPowerups : MonoBehaviour
         playerMovement.externalTopSpeedModifier = normalTopSpeedModifier;
         playerMovement.externalIgnoreInAirAccelerationModifier = false;
         usingTurbo = false;
+
+        // Stop turbo audio
+        playerAudio.StopTurboAudio();
     }
 
     IEnumerator Magnet()
