1a5b8b260ffe568d129de223c1d8ce84f00b11dd
Busmossan <a24emimo@student.his.se>
2026-03-17 10:50:31 +0100

    napoleon looses respect when shooting

--- Assets/Scripts/Player/Character/NapoleonRespect.cs
+++ Assets/Scripts/Player/Character/NapoleonRespect.cs
@@ -13,8 +13,9 @@ public class NapoleonRespect : MonoBehaviour
     [SerializeField] private Image crownfill;
     [SerializeField] private float topSpeed = 100f;
     [SerializeField] private ParticleSystem flames;
+    [SerializeField] private RacerData rd;
     [SerializeField] private bool respectEnabled;      
-    private float speedThreshold = 30f;  
+    private float speedThreshold = 10f;  
     void Start()
     {
         SceneController.instance.SceneChangeEvent += OnSceneLoaded;
@@ -23,8 +24,7 @@ public class NapoleonRespect : MonoBehaviour
     {
         if (!respectEnabled && !SceneController.instance.IsMenu)
         {
-            RacerData data = transform.root.GetComponentInChildren<RacerData>();
-            if (data.racername == "King Napoleon III")
+            if (rd.racername == "King Napoleon III")
             {
                 Respect.SetActive(true);
                 respectEnabled = true;
@@ -39,32 +39,42 @@ public class NapoleonRespect : MonoBehaviour
 
     void Update()
     {
-        if (respectEnabled)
-        {
-            float speed = rb.linearVelocity.magnitude;
-            float normalizedSpeed = Mathf.Clamp01(speed/topSpeed);
-            float inverted = 1f - normalizedSpeed;
-            crownfill.fillAmount = Mathf.Lerp(crownfill.fillAmount, inverted, Time.deltaTime * 5f);
-            if (speed < speedThreshold)
-            {
-                if (!flames.isPlaying)
-                flames.Play();
-            }
-            else
-            {
-                if (flames.isPlaying)
-                flames.Stop();
-                StartCoroutine(LowerRespect());
-            }
+        float speed = rb.linearVelocity.magnitude;
+        bool idle = speed < speedThreshold;
 
+        if (idle)
+        {
+            crownfill.fillAmount -= Time.deltaTime * 0.5f; 
+        }
+        else
+        {
+            crownfill.fillAmount += Time.deltaTime * 0.1f;
+        }
+        crownfill.fillAmount = Mathf.Clamp01(crownfill.fillAmount);
+
+        if (!idle)
+        {
+            if (!flames.isPlaying)
+            flames.Play();
+        }
+        else
+        {
+            flames.Stop();
         }
     }
-    IEnumerator LowerRespect()
+    public IEnumerator LowerRespect()
     {
-        yield return new WaitForSeconds(5f);
+        if (rd.racername == "King Napoleon III")
+        crownfill.fillAmount -= 0.5f; 
+        //return;
+        yield return new WaitForSeconds(1f);
     }
     void OnDisable()
     {
         SceneController.instance.SceneChangeEvent -= OnSceneLoaded;
     }
+    /*float speed = rb.linearVelocity.magnitude;
+            float normalizedSpeed = Mathf.Clamp01(speed/topSpeed);
+            float inverted = 1f - normalizedSpeed;
+            crownfill.fillAmount = Mathf.Lerp(crownfill.fillAmount, 1f, Time.deltaTime * 5f);*/
 }
