e3f8e461f3b66f99c1ce6a70e1abca3db4c2343b
Olle Hasselstrom <olle.choukrihasselstrom@elev.ga.ntig.se>
2026-03-24 15:34:52 +0100

    did not work

--- Assets/ActionCamera.cs
+++ Assets/ActionCamera.cs
@@ -10,9 +10,9 @@ public class ActionCamera : MonoBehaviour
     [SerializeField] private GameObject[] cameraTargets;
     private int currentCamera;
     [SerializeField] public float timer = 5f;
-    private Camera cam;
+    [SerializeField] private Camera cam;
     private int index;
-    
+    private GameObject playerTarget = null;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,7 +27,7 @@ public class ActionCamera : MonoBehaviour
         {
             return;
         }
-        cam = GetComponent<Camera>(); 
+        //cam = GetComponent<Camera>(); 
         cam.rect = new Rect(
             0.5f,
             0,
@@ -45,24 +45,54 @@ public class ActionCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
             ChangeCameraPosition();
             timer = 10;
         }
+        */
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerTarget = other.gameObject;
+            FollowPlayer();
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (playerTarget == other.gameObject)
+        {
+            ChangeCameraPosition();
+        }
     }
 
     
-
-    void ChangeCameraPosition()
+    public void FollowPlayer()
     {
+        Debug.Log("FollowPlayer");
         
-        transform.position = cameraPositions[currentCamera].transform.position;
-        Vector3 direction = cameraTargets[currentCamera].transform.position - transform.position;
-        Vector3 rotation = Vector3.RotateTowards(transform.forward, direction, 90f, 0.0f);
-        transform.rotation = Quaternion.LookRotation(direction);
+        Vector3 direction = playerTarget.transform.position - cam.transform.position;
+        Vector3 rotation = Vector3.RotateTowards(cam.transform.forward, direction, 30f, 0f);
+        cam.transform.rotation = Quaternion.LookRotation(rotation);
+        
+        cam.transform.LookAt(playerTarget.transform.position);
+    }
+
+    public void ChangeCameraPosition()
+    {
+        Debug.Log("ChangeCameraPosition");
+        cam.transform.position = cameraPositions[currentCamera].transform.position;
+        Vector3 direction = cameraTargets[currentCamera].transform.position - cam.transform.position;
+        Vector3 rotation = Vector3.RotateTowards(cam.transform.forward, direction, 90f, 0.0f);
+        cam.transform.rotation = Quaternion.LookRotation(direction);
         currentCamera = (currentCamera + 1) % cameraPositions.Length;
         
     }
